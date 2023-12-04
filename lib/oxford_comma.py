6d5ba7fa48d90138ce72fb96015bb0ba77cd91ce
reagan <reaganm746@gmail.com>
2023-12-04 21:28:20 +0300

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
+
 def oxford_comma(items):
-    return None
+    if not items:
+        return ""
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    oxford_string = ", ".join(items[:-1])  
+    oxford_string += f", and {items[-1]}"  
+    return oxford_string
+
